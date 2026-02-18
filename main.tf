b3cefb4ae24131811f1df19ca5f08fae9ca0ca9f
Abhishek-Jadhav-2618 <abhijadhav2618@gmail.com>
2026-02-18 08:00:45 +0530

    updated az

--- main.tf
+++ main.tf
@@ -6,9 +6,18 @@ resource "aws_vpc" "vpc" {
     }
 }
 
+data "aws_availability_zone" "public-zone" {
+    name = "us-east-1a"
+}
+
+data "aws_availability_zone" "private-zone" {
+    name = "us-east-1b"
+}
+
 resource "aws_subnet" "public_subnet" {
     vpc_id = aws_vpc.vpc.id
     cidr_block = "192.168.1.0/24"
+    availability_zone = data.aws_availability_zone.public-zone.name
     tags = {
         Name = "Public-Subnet"
     }
@@ -17,6 +26,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
     vpc_id = aws_vpc.vpc.id
     cidr_block = "192.168.2.0/24"
+    availability_zone = data.aws_availability_zone.private-zone.name
     tags = {
         Name = "Private Subnet"
     }
@@ -44,6 +54,24 @@ resource "aws_route_table_association" "public-rt-association" {
     route_table_id = aws_route_table.public-RT.id
     subnet_id = aws_subnet.public_subnet.id
 }
+
+resource "aws_route_table" "private-RT" {
+    vpc_id = aws_vpc.vpc.id
+    route{
+        cidr_block = "0.0.0.0/0"
+    }
+    tags = {
+        Name = "Private-RT"
+    }
+}
+
+resource "aws_route_table_association" "private-rt-association" {
+    route_table_id = aws_route_table.private-RT.id
+    subnet_id = aws_subnet.private_subnet.id
+}
+
+
+
 
 
 
